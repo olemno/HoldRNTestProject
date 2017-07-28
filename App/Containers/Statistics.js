31dec97f5b15c0da97e538c3bf32d3efe2290543
olemno <olemno@stud.ntnu.no>
2017-07-28 14:30:50 +0200

    Inital base for test project

---
+++ App/Containers/Statistics.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import styles from '../styles';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Place your magic here!
+        </Text>
+      </View>
+    );
+  }
+}
---
+++ App/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import Statistics from './Containers/Statistics';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Statistics />
+    );
+  }
+}
